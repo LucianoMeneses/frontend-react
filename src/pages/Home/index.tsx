50ef14e1335d3cc52e61405d2d5cf1257cfd4fb1
LUCIANO ARAUJO MENESES <luciano.araujo@cagece.com.br>
2024-11-08 21:07:43 -0300

    feat: adicionando estilização de acordo com tamanho da tela

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,21 @@
+import './styles.css';
 import Navbar from 'components/Navbar';
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/ButtonIcon';
 const Home = () => {
   return (
     <>
       <Navbar />
       <div className="home-container">
-        <div className="home-card"></div>
-        <div className="home-content-container"></div>
-        <h1>Conheça o melhor catálogo de produtos</h1>
-        <div className="home-image-container">
-          <MainImage />
+        <div className="home-card">
+          <div className="home-content-container">
+            <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>Ajudaremos  você a encontrar os melhores produtos disponíveis no mercado</p>
+            <ButtonIcon/>
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
         </div>
       </div>
     </>
